5ecb27f16c13c07a8a1ec31f826f7886e36ea7dd
yuzhirun <ch_yzr@126.com>
2023-12-11 17:13:25 +0800

    test

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import fire
 from processing import *
 from strategy import get_optimizer
 from utils import print_result
-
+# test
+from utils import print_mention
 
 def main(**kwargs):
     train_config, fsdp_config = cuda_communication_init(**kwargs)
+    print('-----------------------rank: {}'.format(RANK))
     model = load_model(cfg=(train_config, fsdp_config))
     tokenizer = load_tokenizer(train_config)
     train_dataloader, test_dataloader = get_dataloader(train_config, tokenizer, **kwargs)
